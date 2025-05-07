de8edd198577c08733064e58086adce391a897a1
Juan Marino Rova <maarino89@hotmail.com>
2025-05-07 20:45:27 -0300

    Creación de estructura

--- football-finder-client/src/App.jsx
+++ football-finder-client/src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Welcome Back!</h1>
-      <h2>Letaaaa</h2>
     </>
   );
 }
