c899feb38576b795333270943d731b9d9621821d
Marino-404 <marinorova25@gmail.com>
2025-05-14 18:47:28 -0300

    Iniciando Login - Register

--- football-finder-client/src/components/styles/Button.jsx
+++ football-finder-client/src/components/styles/Button.jsx
@@ -2,10 +2,9 @@ import { Send } from "lucide-react";
 
 const Button = () => {
   return (
-    <button className="bg-transparent text-white flex items-center gap-2 px-4 py-2 relative group">
+    <button className="bg-transparent border-b-2 border-blue-500 text-white flex items-center gap-2 px-4 py-2 relative group">
       <span className="text-lg">Iniciar</span>
       <Send size={22} strokeWidth={1.75} className="text-blue-500" />
-      <span className="absolute left-0 bottom-0 w-full h-0.5 bg-blue-500 group-hover:h-0.5 transition-all duration-300"></span>
     </button>
   );
 };
