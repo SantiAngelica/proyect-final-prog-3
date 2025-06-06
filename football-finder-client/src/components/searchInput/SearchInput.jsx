8e8c4de7553dda489cb6e3a845087d98ddea8efb
Marino-404 <marinorova25@gmail.com>
2025-06-06 18:52:46 -0300

    1.0

--- football-finder-client/src/components/searchInput/SearchInput.jsx
+++ football-finder-client/src/components/searchInput/SearchInput.jsx
@@ -1,80 +1,30 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React from "react";
 
-const SearchInput = ({query, setQuery}) => {
+const SearchInput = ({ query, setQuery }) => {
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
 
-    const handleChange = (e) => {
-        setQuery(e.target.value);
-    };
-    return (
-        <StyledWrapper>
-            <div className="group">
-                <svg viewBox="0 0 24 24" aria-hidden="true" className="search-icon">
-                    <g>
-                        <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z" />
-                    </g>
-                </svg>
-                <input
-                    className="input"
-                    type="search"
-                    placeholder="Search..."
-                    value={query}
-                    onChange={handleChange}
-                />
-            </div>
-        </StyledWrapper>
-    );
-}
-
-const StyledWrapper = styled.div`
-  .group {
-    display: flex;
-    line-height: 28px;
-    align-items: center;
-    position: relative;
-    max-width: 190px;
-  }
-
-  .input {
-    font-family: "Montserrat", sans-serif;
-    width: 100%;
-    height: 45px;
-    padding-left: 2.5rem;
-    box-shadow: 0 0 0 1.5px #2b2c37, 0 0 25px -17px #000;
-    border: 0;
-    border-radius: 12px;
-    background-color: #16171d;
-    outline: none;
-    color: #bdbecb;
-    transition: all 0.25s cubic-bezier(0.19, 1, 0.22, 1);
-    cursor: text;
-    z-index: 0;
-  }
-
-  .input::placeholder {
-    color: #bdbecb;
-  }
-
-  .input:hover {
-    box-shadow: 0 0 0 2.5px #2f303d, 0px 0px 25px -15px #000;
-  }
-
-  .input:active {
-    transform: scale(0.95);
-  }
-
-  .input:focus {
-    box-shadow: 0 0 0 2.5px #2f303d;
-  }
-
-  .search-icon {
-    position: absolute;
-    left: 1rem;
-    fill: #bdbecb;
-    width: 1rem;
-    height: 1rem;
-    pointer-events: none;
-    z-index: 1;
-  }`;
+  return (
+    <div className="relative flex items-center w-1/3 mx-auto">
+      <svg
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+        className="absolute left-4 w-4 h-4 fill-[#bdbecb] pointer-events-none z-10"
+      >
+        <g>
+          <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z" />
+        </g>
+      </svg>
+      <input
+        type="search"
+        placeholder="Buscar partido..."
+        value={query}
+        onChange={handleChange}
+        className="text-xs text-white font-bold w-full  h-[40px] pl-10 pr-3 rounded-xl border-0 outline-none bg-[#16171d] placeholder-gray-500 shadow-[0_0_0_1.5px_#2b2c37,0_0_25px_-17px_black] transition-all duration-300 ease-out hover:shadow-[0_0_0_2.5px_#2f303d,0_0_25px_-15px_black] focus:shadow-[0_0_0_2.5px_#2f303d] font-montserrat"
+      />
+    </div>
+  );
+};
 
 export default SearchInput;
